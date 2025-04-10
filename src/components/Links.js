f97ecf63d3eb31d2e21212ee29b36784589d02a7
petermunene <peter.munene@student.moringaschool.com>
2025-04-10 11:13:20 +0300

    my commit

---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from "react";
+import About from "./About";
+
+function Links(props){
+    return (
+        <div>
+            <h3>Links</h3>
+            <a href={props.github}>https://github.com/liza</a>
+            <a href={props.linkedin}>https://www.linkedin.com/in/liza/</a>
+
+        </div>
+    )
+}
+
+export default Links
